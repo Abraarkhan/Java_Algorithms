70814882b496b1732a70a944556a8c411d0449e0
Sobhit25 <64360724+Sobhit25@users.noreply.github.com>
2021-10-28 08:47:16 +0530

    memoization.java

---
+++ memoization.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+import java.util.Map;
+ 
+public class FibonacciMemoizationAlgorithm {
+ 
+    private Map<Integer, Integer> memoizeTable = new HashMap<>(); // O(1)
+ 
+    // Fibonacci with Memoization
+    public int fibonacciMemoize(int n){
+ 
+        if( n == 0 ) return 0;
+        if( n == 1 ) return 1;
+ 
+        if( this.memoizeTable.containsKey(n) ) 
+        {
+            System.out.println("Getting value from computed result for "+n);
+            return this.memoizeTable.get(n);
+        }
+ 
+        int result = fibonacciMemoize(n-1)+ fibonacciMemoize(n-2);
+ 
+        System.out.println("Putting result in cache for "+n);
+        this.memoizeTable.put(n, result);
+ 
+        return result;
+ 
+    }
+ 
+    // Fibonacci without Memoization
+    public int fibonacci(int n){
+ 
+        if( n == 0 ) return 0;
+        if( n == 1 ) return 1;
+ 
+        System.out.println("Calculating fibonacci number for: "+n);
+        return (fibonacci(n-1) + fibonacci(n-2));   
+    }
+ 
+    public static void main(String[] args) {
+ 
+        FibonacciMemoizationAlgorithm fibonacciAlgorithm = new FibonacciMemoizationAlgorithm();
+        System.out.println("Fibonacci value for n=5:  "+fibonacciAlgorithm.fibonacciMemoize(5));
+ 
+    }
+}
