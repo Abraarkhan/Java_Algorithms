2e61a21badfcb5ab22803122c3d145db51a35aad
ashirbad1212 <64599641+ashirbad1212@users.noreply.github.com>
2022-10-23 18:06:00 +0530

    animations

---
+++ DSA_JAVA/Online-Survey-System/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Survey_System.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Everything You Wanted to Know, But Were Afraid to Ask";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "SurveyBoard Ltd.";
+
+            return View();
+        }
+    }
+}
